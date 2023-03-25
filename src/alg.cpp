d541098c168b19df375ff5f71a1964ae50ba5088
1pavesh1 <125039843+1pavesh1@users.noreply.github.com>
2023-03-25 22:24:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,8 +15,8 @@ void sort(int *arr, int len) {
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     sort(arr, len);
-    for (int i = 0; i < len; ++i) {
-        for (int j = len - 1; j > i; --j) {
+    for (int i = 0; i < len - 1; ++i) {
+        for (int j = i + 1; j < len - 1; ++j) {
             if (arr[i] + arr[j] == value) {
                 count += 1;
             }
@@ -28,8 +28,8 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
     sort(arr, len);
-    for (int i = 0; i < len; ++i) {
-        for (int j = i - 1; j >= 0; --j) {
+    for (int i = 0; i < len - 1; ++i) {
+        for (int j = len - 1; j > i; --j) {
             if (arr[i] + arr[j] == value) {
                 count += 1;
             }
